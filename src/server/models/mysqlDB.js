adf71e1daf47f7cc76447539c0a62b8c792e086d
bhc8521 <bhc8521>
2022-01-18 17:27:21 +0800

    fix: trying

--- src/server/models/mysqlDB.js
+++ src/server/models/mysqlDB.js
@@ -1,9 +1,20 @@
 const {Sequelize} = require('sequelize');
 const {MYSQL} = require('../../utils/secret.js').secret;
+const Op = Sequelize.Op;
+const operatorsAliases = {
+  $eq: Op.eq,
+  $ne: Op.ne,
+  $in: Op.in,
+  $any: Op.any,
+  $all: Op.all,
+  $values: Op.values,
+  $col: Op.col
+};
 const mySqlPool = new Sequelize(MYSQL.database,MYSQL.user,MYSQL.password,{
     host:MYSQL.host,
     dialect: 'mysql',
-    timezone: '+00:00'
+    timezone: '+00:00',
+    operatorsAliases
 })
 
 
