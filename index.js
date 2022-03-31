924e7822729f200280f5d2cad69b08166ccbbff8
bhc8521 <bhc8521>
2022-03-31 21:16:16 +0800

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('./src/server/server');
 require('./src/server/services/blockService')
 const {timedTask} = require('./src/timedTask/index')
 const {client} =  require('./src/Bot');
-const {secret} = require("./src/utils/secret").getSecret()
+const secret = require("./src/utils/secret").getSecret()
 const {TOKEN} = secret;
 
 async function sleep(ms) {
