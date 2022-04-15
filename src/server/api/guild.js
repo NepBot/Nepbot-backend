c2017baf318b8683a33ce321638140d0b36639a1
bhc8521 <bai_haochen@163.com>
2022-04-15 18:11:46 +0800

    test

--- src/server/api/guild.js
+++ src/server/api/guild.js
@@ -3,7 +3,12 @@ const {client} = require("../../bot");
 
 exports.getMember = (guildId,memberId)=>{
     const guild = this.getGuild(guildId)
-    console.log(guild.members.cache)
+    for (member of guild.members.cache) {
+        console.log(member.user.id)
+        if (member.user.id == memberId) {
+            console.log("found+++++++++++++++++++++++=")
+        }
+    }
     return guild.members.cache.get(memberId);
 }
 exports.getGuild = (guid_id)=>{
