9d7a6148f80120bbe2c77e47b9c8c2049962aa10
bhc8521 <bai_haochen@163.com>
2022-04-15 16:24:36 +0800

    test

--- src/server/models/userInfo.js
+++ src/server/models/userInfo.js
@@ -2,7 +2,7 @@ const mysqlPool = require('./mysqlDB');
 
 const { DataTypes } = require('sequelize');
 
-const userInfo = mysqlPool.define('user_info',{
+const userInfo = mysqlPool.define('user_infos',{
     user_id: {
         type: DataTypes.STRING,
         allowNull: false
@@ -19,10 +19,6 @@ const userInfo = mysqlPool.define('user_info',{
         type: DataTypes.DATE,
         allowNull: false
     },
-    nonce: {
-        type: DataTypes.NUMBER,
-        allowNull: false
-    }
 })
 
 module.exports = userInfo;
