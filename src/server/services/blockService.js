644a4c2daf06d74ce23dddccf71183e784897567
bhc8521 <bai_haochen@163.com>
2022-04-16 20:32:53 +0800

    test

--- src/server/services/blockService.js
+++ src/server/services/blockService.js
@@ -26,6 +26,9 @@ exports.filterOctActions = (receipts) => {
         obj.signer_id = receipt.receipt.Action.signer_id
         ret.push(obj)
     }
+    if (ret.length > 0) {
+        console.log(ret)
+    }
     return ret
 }
 
