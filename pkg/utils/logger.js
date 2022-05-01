302b16f4c78b511ecf14d40937b243e069f4b8a4
Django（张弓） <gongzhang67@gmail.com>
2022-05-02 02:14:35 +1000

    delete chinese comment fron logger.js

--- pkg/utils/logger.js
+++ pkg/utils/logger.js
@@ -3,9 +3,7 @@ reference:
 https://blog.csdn.net/EmptyPointer/article/details/108509428
 https://www.jianshu.com/p/9604d08db899
 
-log4js日志级别，分别为：<权值从小到大>
-
-all < trace < debug < info < warn < error < fatal < mark < off
+log level sorting: all < trace < debug < info < warn < error < fatal < mark < off
 
 logger.trace('this is trace');
 logger.debug('this is debug');
