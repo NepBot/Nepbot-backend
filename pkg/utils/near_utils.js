8bebfeaf80454c1e5734cea8356adba0ce089ce4
bhc8521 <bai_haochen@163.com>
2022-05-04 14:44:52 +0800

    test

--- pkg/utils/near_utils.js
+++ pkg/utils/near_utils.js
@@ -36,7 +36,7 @@ const verifyOperationSign = async (args) => {
 const getSign = async (args) => {
 	const keyStore = config.nearWallet.keyStore;
 	const keyPair = await keyStore.getKey(config.nearWallet.network_id, config.account_id);
-
+	console.log(config.account_id, "==========================================")
 	const args_string = JSON.stringify(args);
 	const data_buffer = Buffer.from(args_string);
 	const { signature } = keyPair.sign(data_buffer);
