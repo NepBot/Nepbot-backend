613fb0e6d9cd1f24530cfb6e96f1fb81da496692
bhc8521 <bai_haochen@163.com>
2022-05-04 13:40:29 +0800

    fix

--- pkg/utils/config.js
+++ pkg/utils/config.js
@@ -10,7 +10,7 @@ const CREDENTIALS_DIR = '.near-credentials';
 const credentialsPath = path.join(homedir, CREDENTIALS_DIR);
 
 const config = require('../../conf/near_net.json');
-config.nearWallet.key_store = new keyStores.UnencryptedFileSystemKeyStore(credentialsPath);
+config.nearWallet.keyStore = new keyStores.UnencryptedFileSystemKeyStore(credentialsPath);
 
 // add the secret config from file (.env) into the following line. same style
 config.bot_token = process.env.BOT_TOKEN;
