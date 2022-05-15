d8668a862f23018cd934a9c7db1a700881b8855d
bhc8521 <bai_haochen@163.com>
2022-05-16 01:45:32 +0800

    fix

--- pkg/models/object/response.js
+++ pkg/models/object/response.js
@@ -4,7 +4,7 @@ module.exports = class {
 	constructor({ code, message, success, data }) {
 		this.code = code || 200;
 		this.message = message || 'success';
-		this.success = success == null || true;
+		this.success = success == null ? true : success
 		this.data = data;
 		logger.info(JSON.stringify(this));
 	}
