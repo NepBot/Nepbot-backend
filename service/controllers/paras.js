9ea7253aa5dd09825d9b1d5787783ddfa3ec2d78
bhc8521 <bai_haochen@163.com>
2022-05-16 18:20:49 +0800

    test

--- service/controllers/paras.js
+++ service/controllers/paras.js
@@ -11,7 +11,7 @@ const createParasCollection = async (ctx, next) => {
     let form = new multiparty.Form();
     const {req, files} = await new Promise((resolve, reject) => {
         form.parse(ctx.req, function (err, fields, files) {
-            console.log(fields)
+            console.log(fields.args)
             resolve({args: JSON.parse(fields.args[0]), files: files})
         })
     })
