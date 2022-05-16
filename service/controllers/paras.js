080e7d713d5fb57467cb758694ccdc0925085275
bhc8521 <bai_haochen@163.com>
2022-05-16 22:17:49 +0800

    test

--- service/controllers/paras.js
+++ service/controllers/paras.js
@@ -29,53 +29,55 @@ var FormData = require('form-data');
 
 const createParasCollection = async (ctx, next) => {
     // console.log("=========================================")
-    // const auth = await nearUtils.genParasAuthToken()
+    const auth = await nearUtils.genParasAuthToken()
+    ctx.redirect(`https://api-v2-${config.nearWallet.networkId}-master.paras.id/collections`)
+    return
     // proxy.web(ctx.req, ctx.res, {
     //     target: `https://api-v2-${config.nearWallet.networkId}-master.paras.id/collections`,
     //     headers: { 'Authorization': auth }
     // }, (e) => {
     //     console.log(e)
     // })
-    // console.log(ctx.res)
+    console.log(ctx.res)
     
-    let form = new multiparty.Form();
-    const {req, files} = await new Promise((resolve, reject) => {
-        form.parse(ctx.req, function (err, fields, files) {
-            resolve({req: JSON.parse(fields.args[0]), files: null})
-        })
-    })
+    // let form = new multiparty.Form();
+    // const {req, files} = await new Promise((resolve, reject) => {
+    //     form.parse(ctx.req, function (err, fields, files) {
+    //         resolve({req: JSON.parse(fields.args[0]), files: null})
+    //     })
+    // })
     
-    const args = req.args;
-    if (!await nearUtils.verifyAccountOwner(req.account_id, args, req.sign)) {
-        logger.error('fn verifyAccountOwner failed in api/setInfo');
-        ctx.body = new Resp({
-            code: 500,
-            message: 'fn verifyAccountOwner failed in api/getOwnerSign',
-            success: false,
-        });
-        return;
-    }
-
-    if (!await nearUtils.verifyOperationSign(args, req.account_id)) {
-		logger.error('fn verifyOperationSign failed in api/get-sign');
-		ctx.body = new Resp({
-			code: 500,
-			message: 'fn verifyOperationSign failed in api/get-sign',
-			success: false,
-		});
-		return;
-	}
-
-    const formData = new FormData();
-    Object.keys(args.args).forEach((key) => {
-        formData.append(key, args.args[key]);
-    });
-    // for (let file of files) {
-    //     const fileObj = fs.readFileSync(file.path)
-    //     formData.append('files',fileObj)
+    // const args = req.args;
+    // if (!await nearUtils.verifyAccountOwner(req.account_id, args, req.sign)) {
+    //     logger.error('fn verifyAccountOwner failed in api/setInfo');
+    //     ctx.body = new Resp({
+    //         code: 500,
+    //         message: 'fn verifyAccountOwner failed in api/getOwnerSign',
+    //         success: false,
+    //     });
+    //     return;
     // }
+
+    // if (!await nearUtils.verifyOperationSign(args, req.account_id)) {
+	// 	logger.error('fn verifyOperationSign failed in api/get-sign');
+	// 	ctx.body = new Resp({
+	// 		code: 500,
+	// 		message: 'fn verifyOperationSign failed in api/get-sign',
+	// 		success: false,
+	// 	});
+	// 	return;
+	// }
+
+    // const formData = new FormData();
+    // Object.keys(args.args).forEach((key) => {
+    //     formData.append(key, args.args[key]);
+    // });
+    // // for (let file of files) {
+    // //     const fileObj = fs.readFileSync(file.path)
+    // //     formData.append('files',fileObj)
+    // // }
     
-    const res = await createCollection(formData, await nearUtils.genParasAuthToken())
+    // const res = await createCollection(formData, await nearUtils.genParasAuthToken())
     ctx.body = new Resp({ 
 		data: ctx.req
 	});
