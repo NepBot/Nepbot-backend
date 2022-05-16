add5dce6290a7d04e81d869e2b36d50387cdf555
bhc8521 <bai_haochen@163.com>
2022-05-16 18:16:19 +0800

    fix

--- service/controllers/paras.js
+++ service/controllers/paras.js
@@ -5,54 +5,56 @@ const discordUtils = require('../../pkg/utils/discord_utils');
 const nearUtils = require('../../pkg/utils/near_utils');
 
 const multiparty = require("multiparty");
+const fs = require("fs")
 
 const createParasCollection = async (ctx, next) => {
     let form = new multiparty.Form();
-    await new Promise((resolve, reject) => {
+    const {req, files} = await new Promise((resolve, reject) => {
         form.parse(ctx.req, function (err, fields, files) {
-            console.log(fields)
-            console.log(files)
-            resolve()
+            resolve({args: JSON.parse(fields.args[0]), files: files})
         })
     })
     
-    // const req = ctx.request.body;
-    // const args = req.args;
-    // if (!await nearUtils.verifyAccountOwner(req.account_id, args, req.sign)) {
-    //     logger.error('fn verifyAccountOwner failed in api/setInfo');
-    //     ctx.body = new Resp({
-    //         code: 500,
-    //         message: 'fn verifyAccountOwner failed in api/getOwnerSign',
-    //         success: false,
-    //     });
-    //     return;
-    // }
+    const args = req.args;
+    if (!await nearUtils.verifyAccountOwner(req.account_id, args, req.sign)) {
+        logger.error('fn verifyAccountOwner failed in api/setInfo');
+        ctx.body = new Resp({
+            code: 500,
+            message: 'fn verifyAccountOwner failed in api/getOwnerSign',
+            success: false,
+        });
+        return;
+    }
 
-    // if (!await nearUtils.verifyOperationSign(args, req.account_id)) {
-	// 	logger.error('fn verifyOperationSign failed in api/get-sign');
-	// 	ctx.body = new Resp({
-	// 		code: 500,
-	// 		message: 'fn verifyOperationSign failed in api/get-sign',
-	// 		success: false,
-	// 	});
-	// 	return;
-	// }
+    if (!await nearUtils.verifyOperationSign(args, req.account_id)) {
+		logger.error('fn verifyOperationSign failed in api/get-sign');
+		ctx.body = new Resp({
+			code: 500,
+			message: 'fn verifyOperationSign failed in api/get-sign',
+			success: false,
+		});
+		return;
+	}
 
-    // const collection = await getCollection(`${args.name}-by-${config.account_id.replace(".", "")}`)
-    // if (!collection || collection.results.length > 0) {
-    //     return
-    // }
-    // const formData = new FormData();
-    // Object.keys(params).forEach((key) => {
-    //     formData.append(key, params[key]);
-    // });
-    // console.log(values,formData,'---formData----');
-    // formData.append('files',values['logo'][0]['originFileObj'])
-    // formData.append('files',values['cover'][0]['originFileObj'])
+    const collection = await getCollection(`${args.collection}-by-${config.account_id}`)
+    if (!collection || collection.results.length > 0) {
+        return
+    }
+    const formData = new FormData();
+    Object.keys(args.args).forEach((key) => {
+        formData.append(key, args.args[key]);
+    });
+    for (let file of files) {
+        const fileObj = fs.createReadStream(file.path)
+        formData.append('files',fileObj)
+    }
     
-
-    // //paras - collection
-    // const res = await createCollection(formData);
+    const res = await request({
+        method: 'POST',
+        url: `https://api-v2-${config.networkId}-master.paras.id/collections`,
+        formData
+    });
+    console.log(res)
     ctx.body = new Resp({ 
 		data: {
 		}
