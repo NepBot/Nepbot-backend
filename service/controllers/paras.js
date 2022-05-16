2a34c0461a8deac5d6b31f46c8b77b9b81f95f85
bhc8521 <bai_haochen@163.com>
2022-05-16 23:18:03 +0800

    test

--- service/controllers/paras.js
+++ service/controllers/paras.js
@@ -39,7 +39,7 @@ const createParasCollection = async (ctx, next) => {
     }, (e) => {
         console.log(e)
     })
-    console.log(ctx.res)
+    //console.log(ctx.res)
     
     // let form = new multiparty.Form();
     // const {req, files} = await new Promise((resolve, reject) => {
