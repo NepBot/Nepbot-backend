d1318880f2702b40fff5e8bce732a06a976d0f0d
Django（张弓） <gongzhang67@gmail.com>
2022-06-01 20:43:52 +1000

    add new function that the user can through click the button to verify their near account in nepbot-join channel

--- service/events/guildCreate.js
+++ service/events/guildCreate.js
@@ -1,9 +1,36 @@
-const { Permissions } = require('discord.js');
+const { MessageEmbed, MessageActionRow, MessageButton, Permissions } = require('discord.js');
+
+const embed = new MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle('Near Wallet Authorization')
+	.setDescription(`Welcome to join this guild.\n 
+  Please use the button below for completing the near wallet authorization operation`);
+
+const button = new MessageButton()
+	.setCustomId('NearConnect')
+	.setLabel('Connect Near Wallet')
+	.setStyle('SECONDARY');
+
+const action = new MessageActionRow()
+	.addComponents(button);
 
 const execute = async guild => {
+	// create nepbot-join channel
 	if (isChannelExists(guild, 'nepbot-join')) {
-		guild.channels.create('nepbot-join');
+		await guild.channels.create('nepbot-join',
+			{ permissionOverwrites: [
+				{
+					id: guild.roles.everyone,
+					allow: [Permissions.FLAGS.VIEW_CHANNEL],
+					deny: [Permissions.FLAGS.SEND_MESSAGES],
+				},
+			] });
+		const channelId = guild.channels.cache.find(channel => channel.name === 'nepbot-join').id;
+		const channel = guild.client.channels.cache.get(channelId);
+		await channel.send({ content: '\n', ephemeral:true, embeds:[embed], components: [action] });
 	}
+
+	// create server owner channle
 	const owner = await guild.fetchOwner();
 	const ownerName = owner.user.username;
 	if (isChannelExists(guild, ownerName)) {
