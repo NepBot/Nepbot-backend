b55baf09957b11f8b8e8f485f5ca089d555e675f
bhc8521 <bhc8521>
2022-06-30 17:29:16 +0800

    test

--- pkg/utils/indexer_utils.js
+++ pkg/utils/indexer_utils.js
@@ -6,7 +6,7 @@ const getTxn = async (guildId) => {
 	const client = await nearIndexerPool.connect();
 	const sqlStr = `
   SELECT
-	DISTINCT(args -> 'args_json' -> 'roles') AS roles, originated_from_transaction_hash AS transaction_hash
+	args -> 'args_json' -> 'roles' AS roles, originated_from_transaction_hash AS transaction_hash
   FROM
 	action_receipt_actions
   RIGHT JOIN receipts ON receipts.receipt_id = action_receipt_actions.receipt_id 
@@ -14,7 +14,7 @@ const getTxn = async (guildId) => {
 	receipt_receiver_account_id = '${config.rule_contract}' and
 	args ->> 'method_name' = 'set_roles' and
 	args -> 'args_json' -> 'roles' -> 0 ->> 'guild_id' = '${guildId}'
-  ORDER BY args -> 'args_json' -> 'timestamp' and args -> 'args_json' -> 'roles' desc`;
+  ORDER BY args -> 'args_json' -> 'timestamp' desc`;
 	const res = await client.query(sqlStr);
 	await client.release();
 	return res.rows
