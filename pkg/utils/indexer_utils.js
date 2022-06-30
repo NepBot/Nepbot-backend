f9520e3db41ea2a62903c0e6251dcd892680ec08
bhc8521 <bhc8521>
2022-06-30 16:22:43 +0800

    test

--- pkg/utils/indexer_utils.js
+++ pkg/utils/indexer_utils.js
@@ -8,7 +8,7 @@ const getTxn = async (guildId) => {
   (SELECT transaction_hash FROM transactions WHERE receiver_account_id = '${ config.rule_contract }') AS a 
   LEFT JOIN transaction_actions AS b ON a.transaction_hash = b.transaction_hash 
   WHERE 
-  	position(b.args -> 'args_json', ${guildId}) > -1
+  	position(b.args -> 'args_json' in ${guildId}) > -1
   ORDER BY b.args -> 'args_json' -> 'timestamp' desc`;
 	const res = await client.query(sqlStr);
 	await client.release();
