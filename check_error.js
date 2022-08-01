50f2ee4327704721bc4aeb898ed1c15a5858c0f5
bhc8521 <bhc8521>
2022-08-01 17:28:25 +0800

    test

--- check_error.js
+++ check_error.js
@@ -8,9 +8,12 @@ const userUtils = require('./pkg/utils/user_utils')
 const discordUtils = require('./pkg/utils/discord_utils.js')
 
 client.on('ready', async () => {
-    setTimeout(() => {
-        console.log(discordUtils.getGuild("945572846275551232"))
-    }, 5000)
+    await new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(discordUtils.getGuild("945572846275551232"))
+            resolve()
+        }, 5000)
+    })
     
     // await userUtils.setUser({guild_id: "945572846275551232", user_id: "880162299992764449"}, "khunpolkaihom.near")
 	process.exit(0);
