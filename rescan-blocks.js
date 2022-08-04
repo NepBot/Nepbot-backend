3d07e7eb3dc2bec9ba255b1325a979b22eb1cd6a
bhc8521 <bhc8521>
2022-08-04 18:13:31 +0800

    test

--- rescan-blocks.js
+++ rescan-blocks.js
@@ -1,5 +1,8 @@
 // Create a new client instance
 const task = require("./service/schedule_task.js")
+const client = require("./service/discord_bot.js")
+client.on("ready", () => {
+    task.scheduleTask(96679790)
+})
 
-task.scheduleTask(96679790)
 
