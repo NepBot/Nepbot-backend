99cbaaf07986395422f4c1b34c0ff8cc93204775
bhc8521 <bhc8521>
2022-08-04 18:11:30 +0800

    test

--- rescan-blocks.js
+++ rescan-blocks.js
@@ -1,14 +1,5 @@
 // Create a new client instance
 const task = require("./service/schedule_task.js")
-const config = require("./pkg/utils/config.js")
 
-const { Client, Intents } = require('discord.js');
-const intents = [Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILDS];
-const client = new Client({ intents: intents });
-
-client.on("ready", () => {
-    task.scheduleTask(96679790)
-})
-
-client.login(config.bot_token)
+task.scheduleTask(96679790)
 
