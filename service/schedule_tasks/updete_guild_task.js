473a40e18b815b61f2712681d63517c671fd4124
bhc8521 <bhc8521>
2022-08-04 19:41:27 +0800

    test

--- service/schedule_tasks/updete_guild_task.js
+++ service/schedule_tasks/updete_guild_task.js
@@ -130,12 +130,12 @@ const update_guild_task = async function(receipts) {
         value: rule.key_field[1],
       });
     }
-    console.log(roles)
     for (const role of roles) {
       try {
         await member.roles.add(role);
       }
       catch (e) {
+        console.log(e)
         continue;
       }
     }
