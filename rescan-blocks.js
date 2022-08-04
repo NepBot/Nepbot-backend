2b5e6803e683fc4b885c12d71be76d0d869f70b0
bhc8521 <bhc8521>
2022-08-04 18:00:41 +0800

    test

--- rescan-blocks.js
+++ rescan-blocks.js
@@ -1,3 +1,6 @@
+const client = require("./service/discord_bot.js")
 const task = require("./service/schedule_task.js")
+const config = require("./pkg/utils/config.js")
+client.login(config.bot_token)
 
 task.scheduleTask(96679790)
