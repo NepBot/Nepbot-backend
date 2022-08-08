fae63695f4ca9f3c9d45eb75596c90a319adcdc2
bhc8521 <bhc8521>
2022-08-08 15:26:12 +0800

    test

--- scripts/re-verify.js
+++ scripts/re-verify.js
@@ -5,7 +5,7 @@ const user_utils = require('../pkg/utils/user_utils');
 
 // Load config info
 const client = require("../service/discord_bot.js")
-client.on("ready", () => {
+client.on("ready", async () => {
     const userInfos = await userInfos_utils.getUsers({
         guild_id: '942411087251398727',
     });
