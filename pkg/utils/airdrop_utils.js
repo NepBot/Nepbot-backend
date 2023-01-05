d3078243e7eee3805198754ceab9e4f20f865e28
bhc8521 <bhc8521>
2023-01-06 00:06:01 +0800

    clear

--- pkg/utils/airdrop_utils.js
+++ pkg/utils/airdrop_utils.js
@@ -32,7 +32,7 @@ exports.formatNFTEmbedMsg = async (interaction) => {
     // else if (field.name == 'End_time(GMT)') {
     //   result.end_time = field.value;
     // }
-    if (field.name == 'Airdrop ID') {
+    if (field.name == 'Airdrop ID' || field.name == "Hash") {
       result.hash = field.value;
     }
   }
@@ -64,7 +64,7 @@ exports.formatFTEmbedMsg = async (interaction) => {
     // else if (field.name == 'End_time(GMT)') {
     //   result.end_time = field.value;
     // }
-    if (field.name == 'Airdrop ID') {
+    if (field.name == 'Airdrop ID' || field.name == "Hash") {
       result.hash = field.value;
     }
   }
