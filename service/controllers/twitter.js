9beb75e8e8e854ba7ec2dc99b489c166b533abfb
bhc8521 <bhc8521>
2023-04-24 18:21:08 +0800

    test

--- service/controllers/twitter.js
+++ service/controllers/twitter.js
@@ -18,35 +18,32 @@ const callback = async (ctx, next) => {
   const state = req.state;
   const code = req.code;
   // Get the saved codeVerifier from session
-  // const codeVerifier = await oauthCache.get({ state: state })
-  //   .then(e => e.code_verifier)
-  //   .catch(() => ctx.body = new Resp({ code: 500 }));
-  // if (!codeVerifier || !state || !code) {
-  //   await oauthCache.delete({ state: state });
-  //   return ctx.body = new Resp({ code: 500 });
-  // }
+  const codeVerifier = await oauthCache.get({ state: state })
+    .then(e => e.code_verifier)
+    .catch(() => ctx.body = new Resp({ code: 500 }));
+  if (!codeVerifier || !state || !code) {
+    await oauthCache.delete({ state: state });
+    return ctx.body = new Resp({ code: 500 });
+  }
 
-  // await twitterClient.loginWithOAuth2({ code, codeVerifier, redirectUri: callbackUrl })
-  //   .then(async ({ client: loggedClient, accessToken, refreshToken, expiresIn }) => {
+  await twitterClient.loginWithOAuth2({ code, codeVerifier, redirectUri: callbackUrl })
+    .then(async ({ client: loggedClient, accessToken, refreshToken, expiresIn }) => {
     // {loggedClient} is an authenticated client in behalf of some user
     // Store {accessToken} somewhere, it will be valid until {expiresIn} is hit.
     // If you want to refresh your token later, store {refreshToken} (it is present if 'offline.access' has been given as scope)
-      // const { data: userObject } = await loggedClient.v2.me();
-      // const expiredAt = await timeUtils.getExpiredTimeBySecond(expiresIn);
-      // const params = { access_token: accessToken, refresh_token: refreshToken, expired_at: expiredAt, twitter_id: userObject.id, twitter_username: userObject.username };
-      // const condition = { state: state };
-      // await twitterUsers.update(params, condition);
-      // await oauthCache.delete({ state: state });
-      // const ruleMsg = await twitterRulesMsg.get({ twitter_state: state });
-      // const dcMsg = await discordUtils.getMessage(ruleMsg.guild_id, ruleMsg.channel_id, ruleMsg.message_id);
-      const result = [{
-        name:'Already in role',
-        value: `✅ You are already in this role .`
-      }]       //await twitterUtils.verifyRuleFromDB(loggedClient, ruleMsg, dcMsg);
+      const { data: userObject } = await loggedClient.v2.me();
+      const expiredAt = await timeUtils.getExpiredTimeBySecond(expiresIn);
+      const params = { access_token: accessToken, refresh_token: refreshToken, expired_at: expiredAt, twitter_id: userObject.id, twitter_username: userObject.username };
+      const condition = { state: state };
+      await twitterUsers.update(params, condition);
+      await oauthCache.delete({ state: state });
+      const ruleMsg = await twitterRulesMsg.get({ twitter_state: state });
+      const dcMsg = await discordUtils.getMessage(ruleMsg.guild_id, ruleMsg.channel_id, ruleMsg.message_id);
+      const result = await twitterUtils.verifyRuleFromDB(loggedClient, ruleMsg, dcMsg);
       await twitterRulesMsg.delete({ twitter_state: state });
       return ctx.body = new Resp({ data: result });
-    // })
-    // .catch(() => ctx.body = new Resp({ code: 500 }));
+    })
+    .catch(() => ctx.body = new Resp({ code: 500 }));
 };
 
 module.exports = {
